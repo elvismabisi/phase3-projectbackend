61a620769532c08eae4f422d3f8cf272d7bd239f
Gregory <elvismabisi@gmail.com>
2023-03-06 13:51:27 +0300

    made new changes application controller file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,69 +1,82 @@
 class ApplicationController < Sinatra::Base
     set :default_content_type, 'application/json'
   
-    # a user should be able to view all their listed projects
+    # Get all users
+    get '/users' do
+      users = User.all
+      users.to_json
+    end
+  
+    # Get a specific user and their projects
+    get '/users/:id' do
+      user = User.find(params[:id])
+      user.to_json(include: :projects)
+    end
+  
+    # Create a new user
+    post '/users' do
+      user = User.create(
+        name: params[:name],
+        email: params[:email],
+        bio: params[:bio]
+      )
+      user.to_json
+    end
+  
+    # Update an existing user
+    patch '/users/:id' do
+      data = JSON.parse(request.body.read)
+      user = User.find(params[:id])
+      user.update(data)
+      user.to_json
+    end
+  
+    # Delete a user
+    delete '/users/:id' do
+      user = User.find(params[:id])
+      user.destroy
+      "User with id #{params[:id]} has been deleted"
+    end
+  
+    # Get all projects
+    get '/projects' do
+      projects = Project.all
+      projects.to_json
+    end
+  
+    # Get a specific project
     get '/projects/:id' do
-      project = User.find(params[:id]).projects
+      project = Project.find(params[:id])
       project.to_json
     end
   
-    # # A user should be able to add a new project to their portfolio.
-    post '/create-project/:id' do
-      project = User.find(params[:id]).projects.create(
+    # Create a new project
+    post '/projects' do
+      project = Project.create(
         title: params[:title],
         description: params[:description],
-        image_url: params[:image_url]
+        image_url: params[:image_url],
+        user_id: params[:user_id]
       )
       project.to_json
-  
     end
   
-    # # A user should be able to update existing project data.
-    patch '/update-project/:id/:id2' do
-      data = JSON.parse(request.body.read) 
-      project = User.find(params[:id]).projects
-      project.find(params[:id2]).update(data)
+    # Update an existing project
+    patch '/projects/:id' do
+      data = JSON.parse(request.body.read)
+      project = Project.find(params[:id])
+      project.update(data)
       project.to_json
     end
   
-    # # A user should be able to delete a project.
-    delete '/delete-project/:id/:id2' do
-      "deleted a project!"
-      project = User.find(params[:id]).projects
-      project.find(params[:id2]).destroy
-     end
-  
-    # # A user should be able to view their listed skills.
-    get '/skills/:id' do
-      skill = User.find(params[:id]).skills
-      skill.to_json
-    end
-  
-    # A user should be able add their skills.
-    # A user can have a maximum of 10 skills. - INCOMPLETE
-    post '/create-skill/:id' do
-      data = JSON.parse(request.body.read) 
-      skill = User.find(params[:id]).skills.create(data)
-      skill.to_json
-    end
-  
-    # A user should be able update their skills.
-    patch '/update-skill/:id/:id2' do
-      data = JSON.parse(request.body.read) 
-      skill = User.find(params[:id]).skills.find(params[:id2]).update(data)
-      skill.to_json
-    end
-  
-    # A user should be able delete their skills.
-    delete '/delete-skill/:id/:id2' do
-      data = JSON.parse(request.body.read)
-      skill = User.find(params[:id]).skills
-      skill.find(params[:id2]).destroy
+    # Delete a project
+    delete '/projects/:id' do
+      project = Project.find(params[:id])
+      project.destroy
+      "Project with id #{params[:id]} has been deleted"
     end
     get '/' do
         "Hello World"
       end
-
-  
-  
   end
+  
